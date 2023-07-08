6c6e254eed2c5d49d9584dde4f0eb8af2137871a
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-07-07 23:14:04 -0400

    added-APIkey

--- assets/script.js
+++ assets/script.js
@@ -9,16 +9,14 @@
     // ^^ This is the input by the user on the search bar
     var populousCities = document.querySelector('populous-cities');
 
-    //https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}.
-    var baseOpenWeatherURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' 
-   
-   // https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
-    var forcast;
-    var lat = 'lat={lat}&';
-    var lon = 'lon={lon}&';
-    var APIkey = 'appid={API key}.';
-    var fullURL = baseOpenWeatherURL + forcast + lat + lon + APIkey;
+   // "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=1f607030bc029e79a2a927c3fe3fb558";
+    var baseOpenWeatherURL = 'http://api.openweathermap.org/data/2.5/weather?q=' 
 
+    var keyAPI = '&appid=1f607030bc029e79a2a927c3fe3fb558'
+
+    var fullURL = baseOpenWeatherURL + cityName + keyAPI;
+
+    
 console.log(fetch('https://api.openweathermap.org/data/2.5/forecast?'));
 
 var searchCityWeatherInput = function(event){ // this function will be called on first when event happens 
@@ -50,16 +48,17 @@ var searchCityWeatherInput = function(event){ // this function will be called on
      
       
       var get 1 Repos = function (cityNmae) { // Once the cityName have been made into the search bar it will activate this function
-        var APIURL = '' + cityName + '/repos'; // the variable will be placed into this URL guiding the user to any repos of the same city 
+        var APIURL = baseOpenWeatherURL + cityName + '/repos'; // the variable will be placed into this URL guiding the user to any repos of the same city 
       
         fetch(APIURL)
           .then(function (response) {
-            if (response.ok) {
+            if (response.cityName) {// .ok is a property
               console.log(response);
+              console.log(response.cityName);
               response.json() // .json will parse the data making it legiable for javascript
               .then(function (data) {  
                 console.log(data);
-                displayRepos(data, user);
+                displayRepos(data, cityName);
               });
             } else {
               alert('Error: ' + response.statusText);
@@ -70,32 +69,48 @@ var searchCityWeatherInput = function(event){ // this function will be called on
           });
       };
 
-    if 
-
-    fetch(fullURL)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
-
+    
 }
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
+var displayRepos = function (repos, searchTerm) {
+    if (repos.length === 0) {
+        openAreaDiv.textContent = '<h4>Enter a location or city in the search bar</h4>' + "<i class=' status-icon icon-'></i>";
+      return;
+    }
+
+    repoSearchTerm.textContent = searchTerm;
+
+  for (var i = 0; i < repos.length; i++) {
+    var repoName = repos[i].owner.login + '/' + repos[i].name;
+
+    var repoEl = document.createElement('a');
+    repoEl.classList = 'list-item flex-row justify-space-between align-center';
+    repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
+
+    var titleEl = document.createElement('span');
+    titleEl.textContent = repoName;
+
+    repoEl.appendChild(titleEl);
+
+    var statusEl = document.createElement('span');
+    statusEl.classList = 'flex-row align-center';
+
+    if (repos[i].open_issues_count > 0) {
+        statusEl.innerHTML =
+          "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+      } else {
+        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+      }
   
-    var username = nameInputEl.value.trim();
+      repoEl.appendChild(statusEl);
   
-    if (username) {
-      getUserRepos(username);
-  
-      repoContainerEl.textContent = '';
-      nameInputEl.value = '';
-    } else {
-      alert('Please enter a GitHub username');
+      repoContainerEl.appendChild(repoEl);
     }
   };
+  
+  userFormEl.addEventListener('submit', formSubmitHandler);
+  languageButtonsEl.addEventListener('click', buttonClickHandler);
+  
 
 /*
 
@@ -108,5 +123,7 @@ Have a search bar that serches city repos
 after everything is set up, have a course of 5 days 
 each with a display of name, date, weather, weather icon,
  wind speed, tempture, and humitaty 
+
+ For each day will be a box and have the stats inside of them
 
 */
